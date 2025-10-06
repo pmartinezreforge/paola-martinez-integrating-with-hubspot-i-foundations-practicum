6cc0e1f68d3b7ad3fb14e13f4e1aca5d4d53ceee
Paola Martínez <paola@reforgesolutions.com>
2025-10-06 14:54:02 -0600

    Create GET route and render Pets custom object in homepage view

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,11 +10,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const CUSTOM_OBJECT_TYPE = process.env.CUSTOM_OBJECT_TYPE;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const petsObject = `https://api.hubspot.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(petsObject, { headers, 
+            params: {
+                properties: 'pet_breed, pet_name, pet_type',
+                limit: 100
+            }
+        });
+
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Pets Collection | HubSpot APIs', petsObject:data});      
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error fetching data");
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
